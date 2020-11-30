7769d62b5a260a6aad87c10e5677cb2968fdce2f
leonel <leonel01sys@gmail.com>
2020-11-30 10:43:51 -0600

    stage 6

--- find_the_word.py
+++ find_the_word.py
@@ -2,17 +2,27 @@ import random
 # Write your code here
 languages = ['python', 'java', 'kotlin', 'javascript']
 answer_word = random.choice(languages)
-missing_words_length = len(answer_word) - 3
-missing_words =  missing_words_length * "-"
-words_help = answer_word[0] + answer_word[1] + answer_word[2] + missing_words
-attempts = 8
+missing_words = len(answer_word) * "-"
+words = list(missing_words)
+chars_answer = list(answer_word)
 
-print("H A N G M A N")    
-print("Guess the word {}: >".format(words_help))
+print("H A N G M A N")
+print()
 
-word = input()
+for _ in range(8):
+    print("".join(words))
+    char = input("Input a letter: ")
+    if char in answer_word:
+        words_repeated = words.count(char)
+        for _ in range(words_repeated):
+            index = chars_answer.index(char)
+            words[index] = char;
+            if(words_repeated > 1):
+                chars_answer[index] = "-"
+                
+    else:
+        print("That letter doesn't appear in the word")
+    print()
 
-if(word == answer_word):
-    print("You survived!")
-else:
-    print("You lost!")
+print("Thanks for playing!")
+print("We'll see how well you did in the next stage")
