560ce874d69d1c3406688668d699c189611e0190
leonel <leonel01sys@gmail.com>
2020-12-02 11:52:08 -0600

    stage 7

--- find_the_word.py
+++ find_the_word.py
@@ -1,28 +1,50 @@
 import random
 # Write your code here
 languages = ['python', 'java', 'kotlin', 'javascript']
-answer_word = random.choice(languages)
-missing_words = len(answer_word) * "-"
-words = list(missing_words)
-chars_answer = list(answer_word)
+answer = random.choice(languages)
+answer_chars = list(answer)
+missing_words = list(len(answer) * "-")
+introduced_chars = list()
+won = False
+attempts = 8
+abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+def addCharactersIntoAnswer(char):
+    words_repeated = answer_chars.count(char)
+    for _ in range(words_repeated):
+        index = answer_chars.index(char)
+        missing_words[index] = char;
+        if(words_repeated > 1):
+            answer_chars[index] = "-"
+# print welcome
 print("H A N G M A N")
-print()
 
-for _ in range(8):
-    print("".join(words))
+# read the input and check it
+while attempts > 0:
+    print()
+    print("".join(missing_words))
     char = input("Input a letter: ")
-    if char in answer_word:
-        words_repeated = words.count(char)
-        for _ in range(words_repeated):
-            index = chars_answer.index(char)
-            words[index] = char;
-            if(words_repeated > 1):
-                chars_answer[index] = "-"
-                
+    if len(char) != 1:
+        print("You should input a single letter")
+    elif char not in abc:
+        print("Please enter a lowercase English letter")
+    elif char in introduced_chars:
+        print("You've already guessed this letter")
+    elif char in answer:
+        addCharactersIntoAnswer(char)
+        introduced_chars.append(char)
     else:
         print("That letter doesn't appear in the word")
-    print()
+        introduced_chars.append(char)
+        attempts -= 1
+    if("".join(missing_words) == answer):
+        won = True;
+        break
+    
+        
 
-print("Thanks for playing!")
-print("We'll see how well you did in the next stage")
+if won == True:
+    print("You guessed the word!")
+    print("You survived!")
+else:
+    print("You lost!")
